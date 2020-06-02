dd3052bee49af603d47dd0215e08d5f47662404d
Peter Han <peterjhan88@gmail.com>
2020-06-02 14:35:54 -0700

    index.html now includes robin.js two questions from myrobin.io included in robin.js

---
+++ robin.js
@@ -0,0 +1,52 @@
+// Fuzzy Slippers
+/*
+Your friend is a veterinarian and wants to know how many pet slippers
+he needs to knit before Christmas.
+Your friend looks after three different species: dogs, cats, and chickens.
+He asks, very politely, if you can write him a function that calculates
+how many slippers he needs to make and provides you with the subtotal of
+each species of animal. Please write a function for your friend that returns
+the number of slippers that he will be knitting!
+*/
+const howManySlippers = (dogs, cats, chickens) => {
+  return (dogs + cats) * 4 + chickens * 2;
+};
+
+// Amazing Anagrams
+/*
+Anagrams are words that contain the exact same letters in the same quantity.
+Robin challenges you to write a function that accepts two words and returns
+true if they are anagrams.
+BONUS: write the function so that it can receive any number of words and
+returns true if all of them are anagrams of each other.
+*/
+const checkAnagram = (firstWord, secondWord) => {
+  if (firstWord.length !== secondWord.length) {
+    return false;
+  }
+  let lettersOfFirstWord = firstWord.split("");
+  let lettersOfSecondWord = secondWord.split("");
+  lettersOfFirstWord = lettersOfFirstWord.sort();
+  lettersOfSecondWord = lettersOfSecondWord.sort();
+  for (let index = 0; index < lettersOfFirstWord.length; index++) {
+    if (lettersOfFirstWord[index] !== lettersOfSecondWord[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+const checkAnagramBonus = (...args) => {
+  const wordLength = args[0].length;
+  for (let index = 0; index < args.length; index++) {
+    if (args[index].length !== wordLength) {
+      return false;
+    }
+  }
+  let wordForComparison = args[0];
+  for (let indexForArgs = 1; indexForArgs < args.length; indexForArgs++) {
+    if (!checkAnagram(wordForComparison, args[indexForArgs])) {
+      return false;
+    }
+  }
+  return true;
+};
