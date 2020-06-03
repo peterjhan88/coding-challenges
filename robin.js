0e4768e1a9f9d4fea7c631c1061f77ea2a317db4
Peter Han <peterjhan88@gmail.com>
2020-06-03 15:32:20 -0700

    robin.js Tax Season answer included

--- robin.js
+++ robin.js
@@ -50,3 +50,14 @@ const checkAnagramBonus = (...args) => {
   }
   return true;
 };
+
+// Tax Season
+/*
+Hip hip hooray! It’s tax season! Your CPA has requested that you provide
+your tax amounts in cents this year.Rather than using pen and paper,
+wouldn’t it be faster for you to write a function that accepts a dollar amount
+and a tax percentage and returns the taxed amount in cents ?
+*/
+const taxAmount = (dollarAmount, taxRate) => {
+  return (dollarAmount * taxRate).toFixed(0);
+};
