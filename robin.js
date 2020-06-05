39491333dd2c5bfebca97cbf79884d945ac6e0dd
Peter Han <peterjhan88@gmail.com>
2020-06-05 12:38:28 -0700

    robin.js answer to Securing Credit Card Number included

--- robin.js
+++ robin.js
@@ -77,3 +77,25 @@ const factorial = num => {
   }
   return num * factorial(--num);
 }
+
+/*
+You've been quarantined and have become addicted to online shopping and
+start hoarding ramen. Much to your surprise, you discover that your favorite
+online marketplace is not secure and displays your credit card details in
+plain text. Being the good samaritan that you are, you decide to write a
+function for them that takes in a credit card number and returns a string
+that replaces all but the last four digits with stars,
+like this: "************1111". Make sure that the number of stars matches
+with the exact number of digits being replaced.
+*/
+const secureCreditCardNumber = cardNumber => {
+  const re = /(\d*)(\d{4,4}$)/;
+  let securedNumber = cardNumber.replace(re, (match, p1, p2) => {
+    let hidingText = "";
+    for (let i = 0; i < p1.length; i++) {
+      hidingText += "*";
+    }
+    return hidingText + p2;
+  });
+  return securedNumber;
+}
