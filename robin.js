bed112ecee31cc7f6643d57de3889e5fe16faf01
Peter Han <peterjhan88@gmail.com>
2020-06-07 11:59:44 -0700

    robin.js answer to Loosee Change included

--- robin.js
+++ robin.js
@@ -125,3 +125,27 @@ const startupName = (nameOne, nameTwo) => {
   newNameArray.push(nameTwo.substring(0, 1).toUpperCase());
   return newNameArray.sort().join("");
 };
+
+
+// Loose Change
+/*
+Having no coffee left in the house, you run out to grab some,
+hastily leaving your wallet behind. You come to this realization
+after the barista hands over your Ristretto, and pull out some
+loose change from your pocket. Embarrassed, you go home empty-handed
+and start on a function to save you next time. Your function will
+accept a total amount due and an array that represents the change in
+your pocket, and holds values in this order: pennies, nickels, dimes,
+quarters. For example, if you provide this array: [1,2,3,4],
+it means that the contents in your pocket consist of
+one penny, two nickels, three dimes, and four quarters.
+If you have enough change to cover your order, the function will return true.
+Otherwise, it'll return false. Good luck!
+*/
+
+const enoughCoins = (amount, changes) => {
+  let inMyPocket =
+    changes[0] + changes[1] * 5 + changes[2] * 10 + changes[3] * 25;
+  // assuming the amount is given in dollar
+  return inMyPocket >= amount * 100;
+};
