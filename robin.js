330f5bd3a29d4adf10d904833c7a6cc19735ffca
Peter Han <peterjhan88@gmail.com>
2020-06-08 11:44:36 -0700

    robin.js answers to Math Wiz AKA Calculator included

--- robin.js
+++ robin.js
@@ -149,3 +149,37 @@ const enoughCoins = (amount, changes) => {
   // assuming the amount is given in dollar
   return inMyPocket >= amount * 100;
 };
+
+
+// Math Wiz AKA The Calculator
+/*
+You're at an in-person interview for a software engineering role that
+you applied to last week. The interview starts off as expected, until
+the hiring manager asks you this: "If I give you any two numbers and
+ask you to add them together, subtract one from the other, multiply
+them, or divide one from the other, could you do it?" You pause at first,
+because you aren't a math genius. But then you realize something - this
+is a technical interview! You could just write a function that receives
+two numbers, an operator ( +, -, x, or / ) and returns the correct answer.
+Right? Let's ace this interview! Don't forget, dividing by 0 is an illegal
+operation, so make sure to return a message when that case appears.
+*/
+const simpleMath = (numberOne, operator, numberTwo) => {
+  let num1 = parseFloat(numberOne);
+  let num2 = parseFloat(numberTwo);
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "x":
+      return num1 * num2;
+    case "/":
+      if (num2 === 0) {
+        return "Dvidiing a nuumber by 0 is not allowed";
+      }
+      return num1 / num2;
+    default:
+      return "Invalid Operator! (+,-,x,/ only!)";
+  }
+};
