57a71fcc8a56878cce4c4e0f29ae57fd7250b1d6
Peter Han <peterjhan88@gmail.com>
2020-06-16 01:28:05 -0700

    robin.js answer to FizzBuzzFuzz included

--- robin.js
+++ robin.js
@@ -463,3 +463,39 @@ const numberPalindrome = (yourNumber) => {
   const palindromeNegative = parseInt(stringNumber, 10);
   return (yourNumber-palindromeNegative)<(palindromePositive-yourNumber)? palindromeNegative : palindromePositive;
 }
+
+
+// FizzBuzZFuzz
+/*
+You've been preparing for your next big interview, and have gone over the classic
+FizzBuzz problem over and over. You decide to visit your Robin dashboard, and it
+looks like the question of the day is not FizzBuzz, but FizzBuzzFuzz! Here are the
+rules:
+Write a function that will write out all the numbers from 1 to 150 to the console.
+For multiples of 3, instead of writing "3", you should write "Fizz".
+When a number is a multiple of "5", you should write "Buzz".
+If a number is a multiple of "7", print "Fuzz".
+For multiples of 7 AND 3, write out "FizzFuzz".
+When a number is a multiple of 7 AND 5, write out "BuzzFuzz".
+When a number is a multiple of 3,5, AND 7, you should write out "FizzBuzZFuzz".
+*/
+const FizzBuzzFuzz = () => {
+  const multi3 = "Fizz";
+  const multi5 = "Buzz";
+  const multi7 = "Fuzz";
+  for(let index=1; index<=150; index++) {
+    let consoleOutput = "";
+    if(index%3==0){
+      consoleOutput += multi3;
+    }
+    if(index%5==0){
+      consoleOutput += multi5;
+    }
+    if(index%7==0){
+      consoleOutput += multi7;
+    }
+    if (index % 3 == 0 || index % 5 == 0 || index % 7 == 0){
+      console.log(index, consoleOutput);
+    };
+  }
+}
