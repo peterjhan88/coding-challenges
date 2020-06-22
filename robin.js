ba7c4e18994ce030a874e1abcc0ba9820f2e02f2
Peter Han <peterjhan88@gmail.com>
2020-06-22 12:38:00 -0700

    robin.js answer to Call In to Win included(Robin's answer is also included)

--- robin.js
+++ robin.js
@@ -676,3 +676,75 @@ const isJackpot = array => {
   }
   return true;
 }
+
+
+// Call In to Win
+/*
+Being locked in during the pandemic, you have somehow managed to watch everything
+on Netflix, Hulu, and probably everything on Youtube as well. You decide to listen
+to your favorite radio station...wait...do you even HAVE a favorite radio station?
+Anyways...the radio host suddenly announces that every caller to dial in within
+the next ten minutes will win a prize! You have no idea what the prize is, so you
+decide to dial this number: "888-YOU-RANG ". Unfortunately, you don't remember which
+letters correspond to which numbers. Why don't you write a function that will take
+a phone number with letters and return it, formatted, with all numbers. It could
+look something like this:
+
+formatPhone("888-YOU-RANG") will return 888-968-7264
+
+Use this chart for reference:
+
+0: none
+
+1: none
+
+2: ABC
+
+3: DEF
+
+4: GHI
+
+5: JKL
+
+6: MNO
+
+7: PQRS
+
+8: TUV
+
+9: WXYZ
+*/
+const formatPhone = string => {
+  const keypad = {
+    "ABC": 2,
+    "DEF": 3,
+    "GHI": 4,
+    "JKL": 5,
+    "MNO": 6,
+    "PQRS": 7,
+    "TUV": 8,
+    "XWYZ": 9
+  }
+  let phoneNumbers = string.split("-").map(
+    word => {
+      if (word.match(/[A-Z]/)) {
+        word = word.split("").map(char => {
+          for (let key in keypad) {
+            let re = new RegExp(char, "gi");
+            if (key.match(re)) {
+              return keypad[key];
+            }
+          }
+        })
+        return word.join("");
+      }
+      return word;
+    }).join("-");
+  return phoneNumbers;
+}
+
+// Robin's
+const formatPhone = phone => {
+  let letters = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUW", "XYZ"];
+  return phone.replace(/[A-Z]/g, (match, i) => letters.indexOf(lettrs.find(e => e.includes(match))) +2);
+}
