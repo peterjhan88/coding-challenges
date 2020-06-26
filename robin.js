dd2cb0523beacfce10f74add0addfe259ace00d9
Peter Han <peterjhan88@gmail.com>
2020-06-26 01:07:49 -0700

    robin.js answer to Supercalifragilisticexpialidocious Vowels included

--- robin.js
+++ robin.js
@@ -802,8 +802,8 @@ const isFullHouse = cards => {
   }
   return triple && pair;
 }
-console.log(isFullHouse(["K", "K", "A", "K", "A"]));
-console.log(isFullHouse(["A", "J", "10", "3", "3"]));
+// console.log(isFullHouse(["K", "K", "A", "K", "A"]));
+// console.log(isFullHouse(["A", "J", "10", "3", "3"]));
 
 // Robin's code
 // function isFullHouse(hand) {
@@ -827,3 +827,18 @@ const multiplicationTable = (x) => {
     }
   }
 };
+
+
+//
+/*
+Supercalifragilisticexpialidocious - now you can say it backwards which is
+Dociousaliexpisticfragicalirupus! But that's going a bit too far, don't you
+think? This is not a string reversal challenge - did you think it was? Instead,
+can you write a function that returns the number of vowels in a string?
+In particular, the team at Robin REALLY wants to know how many vowels are in "
+supercalifragilisticexpialidocious"!
+*/
+const howManyVowels = word => {
+  return word.match(/[aeiou]/gi).length;
+}
+// console.log(howManyVowels('supercalifragilisticexpialidocious'));
