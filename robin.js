c3d4ad2d94fb77166e921a610e127ffcf7bca7c4
Peter Han <peterjhan88@gmail.com>
2020-06-30 12:48:00 -0700

    robin.js answer to Balanceing Act includede

--- robin.js
+++ robin.js
@@ -958,3 +958,23 @@ const inBoxRobin = box => {
   }
   return asterisk === 0 ? false : true;
 }
+
+
+// Balancing Act
+/*
+Have you heard about balanced words? If a word is balanced,
+the sum of the values on the left-hand side of the word is
+equal to the sum of the values on the right-hand side. Each
+letter's value comes from its position in the alphabet
+(a=1, b=2, etc.) and can be summed to determine the left and
+right hand sums. If a word has an odd number of characters,
+the middle character should be ignored. Can you write a function
+that determines whether or not a word is balanced?
+*/
+const isItBalanced = word => {
+  // a=97
+  let midPoint = word.length/2;
+  let leftSide = word.toLowerCase().substring(0, Math.floor(midPoint)).split('').reduce((acc, val) => acc + val.charCodeAt() - 96, 0);
+  let rightSide = word.toLowerCase().substring(Math.round(midPoint, 0)).split('').reduce((acc, val) => acc + val.charCodeAt() - 96, 0);
+  return leftSide === rightSide;
+}
