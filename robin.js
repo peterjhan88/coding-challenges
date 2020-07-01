db11b45163b2372d65d8d6f584bf051f9f959f4e
Peter Han <peterjhan88@gmail.com>
2020-07-01 13:55:32 -0700

    robin.js answer to Amazon Interview Series I included

--- robin.js
+++ robin.js
@@ -978,3 +978,17 @@ const isItBalanced = word => {
   let rightSide = word.toLowerCase().substring(Math.round(midPoint, 0)).split('').reduce((acc, val) => acc + val.charCodeAt() - 96, 0);
   return leftSide === rightSide;
 }
+
+
+// Amazon Interview Series I
+/*
+Robin team recently interviewed a former software engineer at Amazon
+and was able to get some of the actual interview questions asked in
+the last 5 years.
+
+You are given an array of [j1, j2, j3, j4, j5, jn …, k1, k2, k3, k4, k5, kn],
+how would you arrange the given array to [j1, k1, j2, k2, …]?
+*/
+const sortByNumberThenLetter = array => {
+  return array.sort((a, b) => parseInt(a.match(/\d/g).join(''), 10) - parseInt(b.match(/\d/g).join(''), 10));
+}
