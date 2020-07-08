f6fef673f687ad5bc3ef96937b37890725ad8a13
Peter Han <peterjhan88@gmail.com>
2020-07-08 12:09:55 -0700

    robin.js answer to ONLY NUMBERS WELCOMED! included

--- robin.js
+++ robin.js
@@ -1203,3 +1203,27 @@ const thanos = array => {
   }
   return array;
 }
+
+
+// ONLY NUMBERS WELCOMED!
+/*
+How are our lovely number people doing today? We're going to have
+an exclusive numbers-only party next week. You can bring any type
+of friends that wear "String" clothes as long as they are real number.
+As we expect the infinite number of numbers to come to the party, we
+should rather build a scanner that will scan every guest and validate
+whether they are a real numbers. If any number brings a fake guest,
+it will be kicked out of our world! Can your team build a special
+function that will be used in the scanner? Please remember, all guests
+will be wearing string clothes.
+
+For example,
+numScanner("2.2") ➞ true
+numScanner("1208") ➞ true
+numScanner("number") ➞ false
+numScanner("0x71e") ➞ false
+numScanner("2.5.9") ➞ false
+*/
+const numScanner = num => {
+  return num.match(/^(\d+\.|\.)?\d+$/) ? true : false
+}
