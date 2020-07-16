65446431c2e58b505833e35554f1819010e38e27
Peter Han <peterjhan88@gmail.com>
2020-07-16 12:56:19 -0700

    robin.js answer to Hungry Number included

--- robin.js
+++ robin.js
@@ -1409,3 +1409,22 @@ const removeLastVowel = sentence => {
   ).join(' ');
   return desiredSentence;
 }
+
+
+// Hungry Number
+/*
+Do you remember the joke "Why was 6 afraid of 7? Because 7, 8, 9!" ? Well,
+that joke was wrong. A number can only eat the number to the right of it if
+the number is SMALLER. Once it eats that number, it turns into the sum of
+itself and that number. In our case, only one number will be eating - this
+number is the first in the array. You need to create a function that returns
+the final array after this number has finished eating.
+*/
+const numberEatingNumber = numbers => {
+  let sum = numbers[0];
+  let index = 1;
+  while (index < numbers.length && sum > numbers[index]) {
+    sum += numbers[index++];
+  }
+  return [sum, ...numbers.slice(index)];
+}
