5b22524f1627a08dfd50f23452568333c449fe00
Peter Han <peterjhan88@gmail.com>
2020-07-22 14:52:55 -0700

    robin.js answer to Do you love me? included

--- robin.js
+++ robin.js
@@ -1519,3 +1519,34 @@ const countPairs = someString => {
   let pairs = someString.split('').sort().join('').match(/([a-z])\1/gi);
   return pairs ? pairs.length : 0;
 }
+
+
+// Do you love me?
+/*
+You've most likely heard of the game Loves me, loves me not. Someone has a
+flower - each time the pluck off a petal, they say "Loves me"...then the
+next time they pluck off a flower, they say "Loves me not"... and so on and
+so on until they pull off the last petal. Whatever phrase they speak out when
+they pull off the last petal determines whether someone they have interest in
+loves them back or not.
+
+Write a function to simulate this game! Pass in a number of petals.
+Return "Loves me, Loves me not, Loves me...etc". On the LAST phrase,
+write it in ALL CAPS.
+
+doYouLoveMe(4) ==> "Loves me, Loves me not, Loves me, LOVES ME NOT"
+*/
+const doYouLoveMe = numberOfPetals => {
+  let love = "Loves me";
+  let noLove = "Loves me not";
+  let result = [];
+  for (let index = 0; index < numberOfPetals; index++) {
+    if (index % 2 === 0) {
+      result.push(love);
+    } else {
+      result.push(noLove);
+    }
+  }
+  result[numberOfPetals - 1] = result[numberOfPetals - 1].toUpperCase();
+  return result.join(", ");
+}
