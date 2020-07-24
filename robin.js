03427912b39681f5305ffed7146505d6e37edccb
Peter Han <peterjhan88@gmail.com>
2020-07-24 13:05:09 -0700

    robin.js answer to Split the Bill included

--- robin.js
+++ robin.js
@@ -1585,3 +1585,28 @@ const acquaintanceOrFriend = sentence => {
     `
   return result;
 }
+
+
+// Split the Bill
+/*
+You and your friend go out to eat. You order dinner and she orders dessert.
+You decide to split the bill - you pay for the food items and she pays only
+for the dessert items.
+
+If you have two arrays, one with the type of dishes ordered and one with the
+corresponding price, create a function that returns an array where the first
+element is the amount that YOU pay and the second element is that amount that
+YOUR FRIEND pays.
+
+ex)
+
+splitTheBill(["D", "D", "F", "D"], [1, 1, 3, 2]) ==> [3, 4]
+*/
+const splitTheBill = (food, price) => {
+  let bill = [0, 0];
+  for (let index = 0; index < food.length; index++) {
+    let foodType = food[index];
+    foodType === "F" ? bill[0] += price[index] : bill[1] += price[index];
+  }
+  return bill;
+}
