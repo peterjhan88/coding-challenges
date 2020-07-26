1e9cf9e9a3f0ba3218df4b1776a12f4421745389
Peter Han <peterjhan88@gmail.com>
2020-07-26 14:19:42 -0700

    robin.js answer to What Century are YOU Living In? included

--- robin.js
+++ robin.js
@@ -1624,3 +1624,23 @@ const onlyOneOfEach = array => {
   uniqueSet.forEach(element => finalArray.push(element))
   return finalArray;
 }
+
+// What Century are YOU Living In?
+/*
+In trying to come up with an insult for your old-fashioned friend (jokingly, of course),
+you ask "What century are you even from?" This made you think about how to convert a year
+into century format - and you have no idea! After doing a little research, you decide to
+come up with a function to save other people from your misery in the future. Please write
+a function that takes in a year and returns the corresponding century. You will only be
+allowed to input years from 1000 - 2010.
+*/
+const convertToCentury = year => {
+  if (year >= 1000 && year <= 2010) {
+    let century = Math.floor(year / 100) + 1;
+    if (century === 21) {
+      return `${century}st century`;
+    }
+    return `${century}th century`
+  }
+  return "Invalid Input";
+}
