022a4a9c26e3d7e7ea700fefb5a5dac98eec9476
Peter Han <peterjhan88@gmail.com>
2020-08-29 09:58:47 -0700

    robin.js answer to Do yo add up? included

--- robin.js
+++ robin.js
@@ -2301,3 +2301,25 @@ console.log(isDate([1, 2, 4, 0]));
 const isDate = potentialDate => {
   return !isNaN(Date.parse(potentialDate));
 }
+
+// Do you add up ?
+/*
+Robin comes to you today with a challenge about arrays:
+can you write a function that receives two arrays of integers and
+one target integer value and determines if there is a pair of numbers
+that add up to the target value ? One number must come from each array and
+the other must come from the second array.Return true if at least one pair
+exists that adds up to the target value - otherwise, return false.
+
+ex) addUp([1, 2], [2, 3, 5], 4)-- > true
+*/
+const addUp = (arrayOne, arrayTwo, target) => {
+  for(let i=0; i<arrayOne.length; i++){
+    for(let j=0; j<arrayTwo.length; j++){
+      if(arrayOne[i]+arrayTwo[j]===target){
+        return true;
+      }
+    }
+  }
+  return false;
+}
