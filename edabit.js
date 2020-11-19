98e9a92107c6654ea181c58f33d5393277bb1412
Peter Han <peterjhan88@gmail.com>
2020-11-19 10:52:57 -0800

    edabit.js answer to spotlightSum included

--- edabit.js
+++ edabit.js
@@ -2531,3 +2531,28 @@ function hoursPassed(t1, t2) {
     return `${t2Hour - t1Hour} hours`;
   }
 }
+
+function spotlightSum(n) {
+  // https://edabit.com/challenge/PZ7rZh9C47CvYHfN2
+  const theGrid = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+    [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+    [41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+    [51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+    [61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+    [71, 72, 73, 74, 75, 76, 77, 78, 79, 80],
+    [81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+    [91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+  ];
+  let rowIndex = Math.floor(n / 10) - 1;
+  let colIndex = n % 10 - 2;
+  let total = 0;
+  for (let row = rowIndex; row < rowIndex + 3; row++) {
+    for (let col = colIndex; col < colIndex + 3; col++) {
+      total += theGrid[row][col];
+    }
+  }
+  return total;
+}
